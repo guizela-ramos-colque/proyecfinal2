52cf2dd98de5621d32acba2ec17261db408d7603
guizela-ramos-colque <rguizela@gmail.com>
2025-06-16 01:34:06 -0400

    7

---
+++ src/Pie.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function Pie(props) {
+  return (
+    <>
+      <dir className="pie" >
+        <div className="pie1">
+
+          <h4>
+              EVELYN GUIZELA RAMOS COLQUE
+          </h4>
+        </div>
+        <div className="pie2">
+
+        <h2>
+            PROGRAMACION WED 22
+        </h2>
+        </div>
+      </dir>
+    </>
+  );
+}
+
+export default Pie;
